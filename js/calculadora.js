de524fce0d0c993b94aa49fc8de160c5887ffc3b
Giovanne <giovannerolo@outlook.com>
2023-01-20 21:52:36 -0300

    primeiro commit

---
+++ js/calculadora.js
@@ -0,0 +1,10 @@
+export default class Calculadora {
+  constructor(calculadora, kcal) {
+    this.calculadora = document.querySelector(calculadora)
+    this.kcal = document.querySelectorAll(kcal);
+  }
+  incrementar() {
+    const total = this.cal.innertext;
+    
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,4 @@
+import Calculadora from 'Calculadora.js';
+
+const calculadora = new Calculadora('.gramas')
+calculadora.init();
